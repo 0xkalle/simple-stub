92e19d18a4150445c42394c4d31700933301474e
Karl-Heinz Oberkoxholt <support@dev.friiky.eu>
2018-11-06 07:58:11 +0100

    some more tests

--- test/index.js
+++ test/index.js
@@ -58,4 +58,48 @@ describe('simple-stub', () => {
     expect(stubObj.calls.f[2].args[0]).to.equal(3);
     expect(stubObj.calls.f[2].result).to.equal(8);
   });
+
+  it('should rename the calls property as given in options', () => {
+    const obj = {
+      f: a => a + 5,
+    };
+    const stubObj = simpleStub.createStub(obj, { renameCalls: 'myCalls' });
+    stubObj.f(1);
+    expect(stubObj.myCalls.f.length).to.equal(1);
+    expect(stubObj.myCalls.f[0].args[0]).to.equal(1);
+    expect(stubObj.myCalls.f[0].result).to.equal(6);
+  });
+
+  it('should spy on empty functions', () => {
+    const obj = {
+      f: () => { },
+    };
+    const stubObj = simpleStub.createStub(obj);
+    stubObj.f();
+    stubObj.f();
+    expect(stubObj.calls.f.length).to.equal(2);
+  });
+
+  it('should handle multiple functions', () => {
+    const obj = {
+      f: () => { },
+      g: () => { },
+      h: () => { },
+    };
+
+    const stubObj = simpleStub.createStub(obj);
+    stubObj.f(1);
+    stubObj.g(2);
+    stubObj.h(3);
+    stubObj.f(4);
+    expect(stubObj.calls.f.length).to.equal(2);
+    expect(stubObj.calls.f[0].args[0]).to.equal(1);
+    expect(stubObj.calls.f[1].args[0]).to.equal(4);
+
+    expect(stubObj.calls.g.length).to.equal(1);
+    expect(stubObj.calls.g[0].args[0]).to.equal(2);
+
+    expect(stubObj.calls.h.length).to.equal(1);
+    expect(stubObj.calls.h[0].args[0]).to.equal(3);
+  });
 });
